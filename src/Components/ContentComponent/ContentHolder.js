9ebf0ab3a4a2e2a0f1bc070e788d56ae42b75d16
adeoye adela <adeoye.adela@gmail.com>
2019-09-11 23:53:44 +0100

    Added button for select different date

--- src/Components/ContentComponent/ContentHolder.js
+++ src/Components/ContentComponent/ContentHolder.js
@@ -3,7 +3,6 @@ import React from "react";
 const ContentHolder = props => {
   return (
     <div className="content_container" key={props.title}>
-      <h2>Let Me Waoh You With Nature...being {props.date}</h2>
       <h3>Astronomy Picture of the Day</h3>
       <p>{props.title}</p>
       <img src={props.img} alt="Astronomy of the Day"></img>
@@ -12,6 +11,10 @@ const ContentHolder = props => {
       </p>
       <p>Credit: {props.copyright}</p>
     </div>
+
+   
   );
 };
+
+
 export default ContentHolder;
--- src/Components/ContentComponent/Contents.js
+++ src/Components/ContentComponent/Contents.js
@@ -1,26 +1,41 @@
-import React,{useState,useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import ContenHolder from "./ContentHolder";
 import axios from "axios";
 
-
 export default function Contents() {
-    const [apod, setApod] = useState("");
-  
-    useEffect(() => {
-      axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
-        .then(response => {
-          const apod = response.data;
-          console.log(apod.copyright);
-            
-          setApod(apod);
-        console.log(response);
-        })
-        .catch(error => {
-          console.log("Sorry No picture of the Day", error);
-        });
-    }, "");
+  const [apod, setApod] = useState("");
+  const [datePicker, setDate]= useState("");
 
-    return( <ContenHolder key={apod} title={apod.title} copyright={apod.copyright} date={apod.date} explanation={apod.explanation} img={apod.url}/>)
-    
-  }
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY${datePicker}`)
+      .then(response => {
+        const apod = response.data;
+        console.log(apod.copyright);
+
+        setApod(apod);
+        console.log(response);
+      })
+      .catch(error => {
+        console.log("Sorry No picture of the Day or Api limit reached", error);
+      });
+  }, [datePicker]);
+
+  return (
+    <div>
+      <h2>Let Me Waoh You With Nature... Today being {apod.date} or </h2>
+      <input type="date" id="start" name="trip-start"
+      
+       min="2018-01-01" max="2020-12-31"></input> 
+      
+      <ContenHolder
+        key={apod}
+        title={apod.title}
+        copyright={apod.copyright}
+        date={apod.date}
+        explanation={apod.explanation}
+        img={apod.url}
+      />
+    </div>
+  );
+}
